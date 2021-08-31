fa2712208b288d56a9f990f0b656e9341b09d4b9
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-08-31 13:28:34 -0400

    commit tree 226c4f0bc39d08bc544d27fae82f53cff4a82c36

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    
+    function Blur(event){
+        console.log("Hey! Eyes on me!")
+
+    };
+
+    function Focus(event){
+        console.log("Good!")
+    };
+
+    return (
+        <div>
+            <button onBlur={Blur}
+            onFocus= {Focus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
